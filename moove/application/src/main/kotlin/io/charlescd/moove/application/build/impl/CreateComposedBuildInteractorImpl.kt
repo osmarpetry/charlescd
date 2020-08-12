21ff1851052f95a43af5f29108e6aaa4275be0dd
Thalles Freitas <thalles.freitas@zup.com.br>
2020-08-12 16:34:44 -0300

    create test and change version name
    Signed-off-by: Thalles Freitas <thalles.freitas@zup.com.br>

--- moove/application/src/main/kotlin/io/charlescd/moove/application/build/impl/CreateComposedBuildInteractorImpl.kt
+++ moove/application/src/main/kotlin/io/charlescd/moove/application/build/impl/CreateComposedBuildInteractorImpl.kt
@@ -40,6 +40,7 @@ open class CreateComposedBuildInteractorImpl @Inject constructor(
     @Transactional
     override fun execute(request: CreateComposedBuildRequest, workspaceId: String): BuildResponse {
         val build = createBuild(request, workspaceId)
+        println(build)
         return BuildResponse.from(buildService.save(build))
     }
 
@@ -147,19 +148,13 @@ open class CreateComposedBuildInteractorImpl @Inject constructor(
         moduleSnapshot: ModuleSnapshot,
         request: CreateComposedBuildRequest
     ): ModuleSnapshot {
-        println(moduleSnapshot.copy(
-            components = moduleSnapshot.components.map { componentSnapshot ->
-                copyComponentSnapshot(
-                    componentSnapshot,
-                    findComponentRequest(module.id, componentSnapshot.componentId, request)
-                )
-            }
-        ))
+
         return moduleSnapshot.copy(
             components = moduleSnapshot.components.map { componentSnapshot ->
                 copyComponentSnapshot(
                     componentSnapshot,
-                    findComponentRequest(module.id, componentSnapshot.componentId, request)
+                    findComponentRequest(module.id, componentSnapshot.componentId, request),
+                    request.releaseName
                 )
             }
         )
@@ -167,23 +162,26 @@ open class CreateComposedBuildInteractorImpl @Inject constructor(
 
     private fun copyComponentSnapshot(
         componentSnapshot: ComponentSnapshot,
-        componentRequest: CreateComposedBuildRequest.ComponentRequest
+        componentRequest: CreateComposedBuildRequest.ComponentRequest,
+        releaseName: String
     ): ComponentSnapshot {
         return componentSnapshot.copy(
             artifact = createArtifactSnapshot(
                 componentSnapshot,
-                componentRequest
+               componentRequest,
+                releaseName
             )
         )
     }
 
     private fun createArtifactSnapshot(
         componentSnapshot: ComponentSnapshot,
-        componentRequest: CreateComposedBuildRequest.ComponentRequest
+        componentRequest: CreateComposedBuildRequest.ComponentRequest,
+        releaseName: String
     ) = ArtifactSnapshot(
         UUID.randomUUID().toString(),
         componentRequest.artifact,
-        componentRequest.version,
+        releaseName,
         componentSnapshot.id,
         LocalDateTime.now()
     )
